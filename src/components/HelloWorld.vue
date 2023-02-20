aa199cf39411cbe29fe265e8e4a979e83452dad3
Bar Noa Atias <noatias1234@gmail.com>
2023-02-20 14:30:51 +0200

    Watchers

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <h2>Computed full name - {{ fullName }}</h2>
-    <h2>Computed total - {{ total }}</h2>
-    <button @click="changeFullName">Change full name</button>
-    <h2 v-for="item in expensiveItems" :key="item.id">
-      {{ item.title }} {{ item.price }}</h2>
+    <h2>Volume tracker (0-20)</h2>
+    <h3>Current volume - {{ volume }}</h3>
+    <button @click="this.volume += 2">Increase volume</button>
   </div>
 </template>
 
@@ -14,47 +12,15 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      firstName: 'Sam',
-      lastName: 'David',
-      items: [
-        {
-          id: 1,
-          title: 'TV',
-          price: 300
-        }, {
-          id: 2,
-          title: 'Phone',
-          price: 1000
-        },
-        {
-          id: 1,
-          title: 'Laptop',
-          price: 500
-        },
-      ]
+      volume: 0
     }
   },
-  methods: {
-    changeFullName() {
-      this.fullName = 'Nate Rosen'
-    }
-  },
-  computed: { //Computed properties are automaticly recalculated if their dependencies change , Perfer if we have a big logic 
-    fullName: {
-      get() {
-        return this.firstName + " " + this.lastName
-      },
-      set(value) {
-        const names = value.split(' ');
-        this.firstName = names[0];
-        this.lastName = names[1];
-      }
-    },
-    total() {
-      return this.items.reduce((total, curr) => (total = total + curr.price), 0);
-    },
-    expensiveItems() {
-      return this.items.filter(item => item.price > 400);
+  methods: {},
+  computed: {},
+  watch: { // watch for a change in value , in this way we can react to data changes 
+    volume(newValue, oldValue) {
+      if (oldValue < newValue && newValue === 16)
+        alert("Listening to a high volume for a long time may damage your hearing");
     }
   }
 }
