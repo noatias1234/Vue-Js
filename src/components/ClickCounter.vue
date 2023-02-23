656b39c9b0cb4006d0614c2074b563603b5e96a9
Bar Noa Atias <noatias1234@gmail.com>
2023-02-23 16:54:45 +0200

    Reusability with mixins

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <button @click="increase">Clicked {{ count }} times</button>
+    </div>
+</template>
+
+<script>
+import counter from '@/mixins/counter';
+export default {
+    name: `ClickCounter`,
+    mixins: [counter]
+
+}
+</script>
+  
+<style scoped></style>
---
+++ src/components/HoverCounter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2 @mouseover="increase">Clicked {{ count }} times</h2>
+    </div>
+</template>
+
+<script>
+import counter from '@/mixins/counter';
+export default {
+    name: `HoverCounter`,
+    mixins: [counter]
+
+}
+</script>
+
+<style scoped></style>
