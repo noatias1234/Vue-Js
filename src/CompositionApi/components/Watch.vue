d972a7dfbb8be12a36263980473441b53e08b35b
Bar Noa Atias <noatias1234@gmail.com>
2023-03-02 13:18:15 +0200

    Replace watcher - reactive and deep watch

--- src/CompositionApi/components/Watch.vue
+++ src/CompositionApi/components/Watch.vue
@@ -3,18 +3,60 @@
         <input type="text" placeholder="First name" v-model="firstName">
         <input type="text" placeholder="Last name" v-model="lastName">
 
+        <input type="text" placeholder="Reactive - First name" v-model="fName">
+        <input type="text" placeholder="Reactive - Last name" v-model="lName">
+        <input type="text" placeholder="Hero Name" v-model="options.heroName">
+
     </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
+import { ref, watch, reactive, toRefs } from 'vue'
+import _ from 'lodash'
+
 export default {
     name: `Watch`,
     setup() {
         const firstName = ref('')
         const lastName = ref('Wayne')
 
+        const state = reactive({
+            fName: '',
+            lName: '',
+            options: {
+                heroName: ''
+            }
+        })
 
+        //2 deep options to watch nested properties
+        watch(() => state.options,
+            function (newValue, oldValue) {
+                console.log('Old value:', oldValue);
+                console.log('New value:', newValue);
+
+            },
+            {
+                deep: true,
+            })
+
+        // yarn add loadash
+        watch(() => _.cloneDeep(state.options),
+            function (newValue, oldValue) {
+                console.log('Old value:', oldValue);
+                console.log('New value:', newValue);
+
+            })
+
+        watch(() => {
+            return { ...state }
+        },
+            function (newValue, oldValue) {
+                console.log('fName old value:', oldValue.fName);
+                console.log('fName new value:', newValue.fName);
+                console.log('lName old value:', oldValue.lName);
+                console.log('lName new value:', newValue.lName);
+
+            })
         watch(
             [firstName, lastName],
             (newValues, oldValues) => {
@@ -30,7 +72,8 @@ export default {
 
         return {
             firstName,
-            lastName
+            lastName,
+            ...toRefs(state)
         }
     }
 }
